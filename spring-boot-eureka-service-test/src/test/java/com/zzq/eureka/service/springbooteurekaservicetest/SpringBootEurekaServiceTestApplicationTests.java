4ed2e185b1f25148b9d2e4c9dfadd36c0728d6ff
zhouzhongqing <1030907690@qq.com>
2018-11-13 16:06:57 +0800

    消费者

---
+++ spring-boot-eureka-service-test/src/test/java/com/zzq/eureka/service/springbooteurekaservicetest/SpringBootEurekaServiceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzq.eureka.service.springbooteurekaservicetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootEurekaServiceTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
